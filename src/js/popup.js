a1d59799dcd2b849b3bfb48084f6b2cc51996686
PRISM <36479341+alsokpisz@users.noreply.github.com>
2018-11-02 06:54:40 -0500

    On button press, query form stores into local js variable, displays in HTML below. Also added toggle to query display

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,30 @@
 import "../css/popup.css";
 import hello from "./popup/example";
-console.log("Hello test");
+function openOutput() {
+    document.getElementById("output").classList.toggle("output-open");
+} //define openOutput to toggle CSS class
+document.getElementById("output-button").addEventListener('click',openOutput); //tells button to use openOutput on click
+//----------------------
+var queryString;
+function saveQuery() {
+    localStorage.setItem('receivedQuery', queryString); //This function saves the input query to a js variable in local storage
+}
+var queryString = localStorage.getItem('receivedQuery'); //put the 'received query' data into the var queryString
+if (queryString == null) {
+    queryString = "Please input a search into the query section!"; //If the var queryString is empty, fill it with this
+}
+function getQuery() { //get query from form when submitted and store in local
+    queryString = document.getElementById("query-form").value;
+    saveQuery();
+    dispQuery();
+}
+document.getElementById("query-form").addEventListener('submit',function(e) { //On query submit click, retrieve, store, and display the query
+    e.preventDefault()
+    getQuery();
+});
+
+function dispQuery() {
+    document.getElementById("output-paragraph").innerHTML = 'Your query was: ${queryString}.';
+}
 
 
