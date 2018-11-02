89de62a94feb8c46454f97ba365d23478dea7c55
PRISM <36479341+alsokpisz@users.noreply.github.com>
2018-11-02 06:32:22 -0500

    Trying a new basic approach by adding a query form and making a simplistic AJAX call, first steps

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,15 +1,5 @@
 import "../css/popup.css";
 import hello from "./popup/example";
-let submit = document.getElementById('submit');
-
-submit.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'console.log(color)'});
-            //{code: 'document.body.style.backgroundColor = "' + color + '";'}); //Change this line... everything else should stay..?
-    });
-};
+console.log("Hello test");
 
 
