3684781cd3d083f5135860019ed59c2f612463f1
PRISM <36479341+alsokpisz@users.noreply.github.com>
2018-11-04 16:42:00 -0600

    Trying to fix 'undefined' string

--- src/js/popup.js
+++ src/js/popup.js
@@ -10,26 +10,27 @@ function openOutput() {
 // ^^^ per https://stackoverflow.com/questions/19074171/how-to-toggle-a-divs-visibility-by-using-a-button-click
 document.getElementById("output-button").addEventListener('click', openOutput); //tells button to use openOutput on click
 //----------------------
-var queryString;
+var queryString = localStorage.getItem('receivedQuery');
+
 function saveQuery() {
     localStorage.setItem('receivedQuery', queryString); //This function saves the input query to a js variable in local storage
 }
-var queryString = localStorage.getItem('receivedQuery'); //put the 'received query' data into the var queryString
-if (queryString == null) {
-    queryString = "Please input a search into the query section!"; //If the var queryString is empty, fill it with this
-}
+
 function getQuery() { //get query from form when submitted and store in local
     queryString = document.getElementById("query-form").value;
+    if (queryString == null) {
+        queryString = "Please input a search into the query section!"; //If the var queryString is empty, fill it with this
+    }
     saveQuery();
     dispQuery();
 }
-document.getElementById("query-form").addEventListener('submit',function(e) { //On query submit click, retrieve, store, and display the query
+document.getElementById("query-button").addEventListener('submit',function(e) { //On query submit click, retrieve, store, and display the query
     e.preventDefault()
     getQuery();
 });
 
 function dispQuery() {
-    document.getElementById("output-paragraph").innerHTML = "Your query was: " + queryString +".";
+    document.getElementById("output-paragraph").innerHTML = "Your query was: " + queryString + ".";
 }
 
 
