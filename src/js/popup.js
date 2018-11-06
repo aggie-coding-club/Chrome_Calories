921795bfe2f9d3e1a46a3236aed484c785fadcd8
PRISM <36479341+alsokpisz@users.noreply.github.com>
2018-11-05 21:09:25 -0600

    Working on adding Nutritionix fallback API functionality

--- src/js/popup.js
+++ src/js/popup.js
@@ -38,7 +38,7 @@ function dispQuery() {
 
 function displayQuery() {
     var request = new XMLHttpRequest(); //div request-output
-    request.open('GET', 'https://api.edamam.com/api/food-database/parser?ingr='+queryString+'&app_id=1111f66d&app_key=11111dd962aa7f5edaef3461a748bd2f');
+    request.open('GET', 'https://api.edamam.com/api/food-database/parser?ingr='+queryString+'&app_id=2f21f66d&app_key=b16d1dd962aa7f5edaef3461a748bd2f');
     request.onload = function() {
         try {
             var data = JSON.parse(request.responseText);
@@ -50,9 +50,27 @@ function displayQuery() {
             document.getElementById("3").innerHTML = "Calories: " + data["parsed"][0]["food"]["nutrients"]["ENERC_KCAL"];// + ", Interpretation: " + data.parsed.food.label + ", Calories: " + data.parsed.food.nutrients.ENERC_KCAL + ".";
         }
         catch(err) {
+            /* Commented out to be replaced with fallback
             document.getElementById("1").innerHTML = "Input text: " + data["text"];
             document.getElementById("2").innerHTML = "Interpretation: Error";
             document.getElementById("3").innerHTML = "Calories: Error";
+            */
+           request.open('POST', ' https://trackapi.nutritionix.com/v2/natural/nutrients');
+           request.setRequestHeader('x-app-id','00004f72')
+           request.setRequestHeader('x-app-key','0000ff353e471806abb26fe29df37a60')
+           request.setRequestHeader('x-remote-user-id','0')
+           request.setRequestHeader('Content-Type','application/json')
+           //https://blog.garstasio.com/you-dont-need-jquery/ajax/#posting
+           //https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit
+           var jsonData = { query: ""+queryString+"" }
+           request.onload = function() {
+               try {
+                   var myJSON = JSON.stringify(jsonData);
+               }
+               catch(err) {
+                //IN PROGRESS
+               }
+           }
         }
     }
     request.send();
