5246db3a027b9986d93d58ede7e6373d4f4c2dda
Ryan Conn <rconn478@gmail.com>
2018-11-19 10:34:38 -0600

    Added specific case for allrecipes

--- src/js/scraping.js
+++ src/js/scraping.js
@@ -3,6 +3,17 @@
 const cheerio = require('cheerio');
 const request = require('request');
 function getIngredients(url) {
+    /*try to resolve to a specific domain that runs incorrectly,
+    use generic code to resolve ingredients for websites not
+    accounted for*/
+    //allrecipes.com
+    if (url.indexOf("allrecipes.com") != -1)
+        getFromAllRecipes(url);
+    else
+    getFromGenericURL(url);
+}
+
+function getFromGenericURL(url) {
     request(url, (error, response, html) => {
         if (!error && response.statusCode == 200) {
             const $ = cheerio.load(html);
@@ -14,21 +25,37 @@ function getIngredients(url) {
                 if ($(el).is("ul")) {
                     $(el).find("li").each(function() {
                         const text = $(this).text().trim();
-                        if (/^[aA0-9]+ *[/0-9a-zA-Z]+/.test(text))
+                        if (text.length > 0 && text.length < 80)
                             ingredients.push(text);
                     })
                 }
                 else {
                     const text = $(el).text().trim();
                     //ingredient quantities should start with numbers and be followed by text
-                    if (/^[aA0-9]+ *[/0-9a-zA-Z]+/.test(text))
+                    if (text.length > 0 && text.length < 80)
                         ingredients.push(text);
                 }
             })
-            ingredients = ingredients.join('\n');
             console.log(ingredients);
             return ingredients;
         }
     });
 }
-const ingredients = getIngredients('https://www.tasteofhome.com/recipes/southwestern-casserole/');
+
+function getFromAllRecipes(url) {
+    request(url, (error, response, html) => {
+        const $ = cheerio.load(html);
+        let ingredients = [];
+        let ingredientsTextEls = $('.recipe-ingred_txt');
+        ingredientsTextEls.each(function() {
+            const text = $(this).text().trim();
+            //allrecipes has elements that say "add all ingredients to list"
+            //with the same class name as ingrdients, so filter them out
+            if (text.indexOf("Add all ingredients") == -1)
+                ingredients.push(text);
+        })
+        console.log(ingredients);
+    });
+}
+
+const ingredients = getIngredients('https://www.geniuskitchen.com/recipe/perfect-roast-turkey-104958');
