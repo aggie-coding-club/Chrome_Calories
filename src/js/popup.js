cff14a5e96403672124e881fab4f1a7b6fb0c0d2
rubenuribe-2 <uriben2000@gmail.com>
2019-03-31 16:36:11 -0500

    slight code refactoring

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,7 +1,7 @@
 
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import "./../css/popup.css";
+import "./../css/popup.css";//stylesheet for popup
 import hello from "./popup/example";
 //[FRONTEND]
 
