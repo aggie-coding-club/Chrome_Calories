153ea49bade29629a49a811ad2e9790c61c7c213
rubenuribe-2 <uriben2000@gmail.com>
2019-03-31 16:33:33 -0500

    mint background & functionality

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,6 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import "./../css/popup.css";
 import hello from "./popup/example";
 //[FRONTEND]
